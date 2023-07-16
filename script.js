b81697bbecffc8dbeab644f543e80779378b377e
youseef khaled <131982745+Yousef582000@users.noreply.github.com>
2023-07-16 22:16:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const count = document.querySelector('#count');
+const incrementButton = document.querySelector('#increment');
+const resetButton = document.querySelector('#reset');
+let counter = 0;
+
+incrementButton.addEventListener('click', () => {
+  counter++;
+  count.textContent = counter;
+});
+
+resetButton.addEventListener('click', () => {
+  counter = 0;
+  count.textContent = counter;
+});
+const count1 = document.querySelector('#count1');
+const incrementButton1 = document.querySelector('#increment1');
+const resetButton1 = document.querySelector('#reset1');
+
+let counter1 = 0;
+
+incrementButton1.addEventListener('click', () => {
+  counter++;
+  count1.textContent = counter;
+});
+
+resetButton1.addEventListener('click', () => {
+  counter = 0;
+  count1.textContent = counter;
+});
+
+const count2 = document.querySelector('#count2');
+const incrementButton2 = document.querySelector('#increment2');
+const resetButton2 = document.querySelector('#reset2');
+
+let counter2 = 0;
+
+incrementButton2.addEventListener('click', () => {
+  counter++;
+  count2.textContent = counter;
+});
+
+resetButton2.addEventListener('click', () => {
+  counter = 0;
+  count2.textContent = counter;
+});
+
+
+const count3 = document.querySelector('#count3');
+const incrementButton3 = document.querySelector('#increment3');
+const resetButton3 = document.querySelector('#reset3');
+
+let counter3 = 0;
+
+incrementButton3.addEventListener('click', () => {
+  counter++;
+  count3.textContent = counter;
+});
+
+resetButton3.addEventListener('click', () => {
+  counter = 0;
+  count3.textContent = counter;
+});
+
+const count4 = document.querySelector('#count4');
+const incrementButton4 = document.querySelector('#increment4');
+const resetButton4 = document.querySelector('#reset4');
+
+let counter4 = 0;
+
+incrementButton4.addEventListener('click', () => {
+  counter++;
+  count4.textContent = counter;
+});
+
+resetButton4.addEventListener('click', () => {
+  counter = 0;
+  count4.textContent = counter;
+});
